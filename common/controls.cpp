7f2f51a4f4c128c39c341764a06d1501052ff5bc
FadyNasser <fady_nasser9@hotmail.com>
2017-12-16 03:20:15 +0200

    Add files via upload

--- common/controls.cpp
+++ common/controls.cpp
@@ -1,17 +1,20 @@
-// Include GLFW
 #include <GL/glfw.h>
-
-// Include GLM
+#include <iostream>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 using namespace glm;
+using namespace std;
 
 #include "controls.hpp"
-
+float rightFactor;
+float fuel = 10000;
+bool fuelRunOut = false;
 glm::mat4 ViewMatrix;
 glm::mat4 ProjectionMatrix;
 glm::vec3 position = glm::vec3(0, 0, 5);
-glm::mat4 getViewMatrix() {
+glm::vec3 SSPosition = glm::vec3(0, 0.5, 8);
+glm::mat4 getViewMatrix() 
+{
 	return ViewMatrix;
 }
 glm::mat4 getProjectionMatrix() {
@@ -23,6 +26,14 @@ glm::vec3 getCameraPosition()
 	return position;
 }
 
+glm::vec3 getSSPosition()
+{
+	// SSPosition.x = position.x;
+	SSPosition.y = position.y - 0.5f;
+	SSPosition.z = position.z - 3.0f;
+	return SSPosition;
+}
+
 // Initial horizontal angle : toward -Z
 float horizontalAngle = 3.14f;
 // Initial vertical angle : none
@@ -30,9 +41,10 @@ float verticalAngle = 0.0f;
 // Initial Field of View
 float initialFoV = 45.0f;
 
-float speed = 3.0f; // 3 units / second
+float speed = 40.0f; // 3 units / second
 float mouseSpeed = 0.005f;
-
+bool wasPressed = false;
+float factor = 0.0f;
 
 void computeMatricesFromInputs() {
 
@@ -56,40 +68,82 @@ void computeMatricesFromInputs() {
 	verticalAngle = mouseSpeed * float(768 / 2 - ypos);
 
 	// Direction : Spherical coordinates to Cartesian coordinates conversion
-	glm::vec3 direction(
-		cos(verticalAngle) * sin(horizontalAngle),
+	glm::vec3 direction
+	(
+		0,
 		sin(verticalAngle),
 		cos(verticalAngle) * cos(horizontalAngle)
-		);
+	);
 
 	// Right vector
-	glm::vec3 right = glm::vec3(
+	glm::vec3 right = glm::vec3
+	(
 		sin(horizontalAngle - 3.14f / 2.0f),
 		0,
 		cos(horizontalAngle - 3.14f / 2.0f)
-		);
+	);
 
 	// Up vector
 	glm::vec3 up = glm::cross(right, direction);
+	//if was pressed last time, increment and multiply by speed as a range from 0 to 1, else start over.
 
 	// Move forward
-	if (glfwGetKey(GLFW_KEY_UP) == GLFW_PRESS) {
-        position += up * deltaTime * speed;
+	if (glfwGetKey(GLFW_KEY_UP) == GLFW_PRESS) 
+	{
+		position += up * deltaTime * speed;
 	}
-    if (glfwGetKey(GLFW_KEY_SPACE) == GLFW_PRESS) {
-        position += direction * deltaTime * speed;
-    }
+	if (glfwGetKey(GLFW_KEY_SPACE) == GLFW_PRESS) 
+	{
+		if (wasPressed && factor < 1) 
+		{
+			factor += 0.0004;
+		}
+		else if (!wasPressed)
+		{
+			factor = 0.0;
+		}
+		wasPressed = true;
+		if (!fuelRunOut)
+		{
+			position += direction * deltaTime * speed * factor;
+		}
+	}
+	if (glfwGetKey(GLFW_KEY_SPACE) != GLFW_PRESS && factor > 0) 
+	{
+		factor -= 0.0001;
+		if (!fuelRunOut)
+		{
+			position += direction * deltaTime * speed * factor;
+		}
+	}
+	fuel -= factor;
+	if (fuel <= 0) 
+	{
+		cout << "run out of fuel"; 
+		fuelRunOut = true;
+	}
 	// Move backward
-	if (glfwGetKey(GLFW_KEY_DOWN) == GLFW_PRESS) {
-        position -= up * deltaTime * speed;
+	if (glfwGetKey(GLFW_KEY_DOWN) == GLFW_PRESS) 
+	{
+		position -= up * deltaTime * speed;
 	}
 	// Strafe right
-	if (glfwGetKey(GLFW_KEY_RIGHT) == GLFW_PRESS) {
-		position += right * deltaTime * speed;
+	if (glfwGetKey(GLFW_KEY_RIGHT) == GLFW_PRESS) 
+	{
+		if (rightFactor <= 15) 
+		{
+			rightFactor += 0.1;
+			SSPosition += right * deltaTime * speed*0.2f;
+		}
 	}
 	// Strafe left
-	if (glfwGetKey(GLFW_KEY_LEFT) == GLFW_PRESS) {
-		position -= right * deltaTime * speed;
+	if (glfwGetKey(GLFW_KEY_LEFT) == GLFW_PRESS) 
+	{
+		if (rightFactor >= -13) 
+		{
+			rightFactor -= 0.1;
+			SSPosition -= right * deltaTime * speed*0.2f;
+		}
 	}
 
 	float FoV = initialFoV - 5 * glfwGetMouseWheel();
@@ -97,11 +151,12 @@ void computeMatricesFromInputs() {
 	// Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
 	ProjectionMatrix = glm::perspective(FoV, 4.0f / 3.0f, 0.1f, 100.0f);
 	// Camera matrix
-	ViewMatrix = glm::lookAt(
+	ViewMatrix = glm::lookAt
+	(
 		position,           // Camera is here
 		position + direction, // and looks here : at the same position, plus "direction"
 		up                  // Head is up (set to 0,-1,0 to look upside-down)
-		);
+	);
 
 	// For the next frame, the "last time" will be "now"
 	lastTime = currentTime;
